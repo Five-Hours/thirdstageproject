d39f8c54b2cc2fdc8e945bd507c3cd50ee7bacc6
zs <zs@qq.com>
2020-03-02 16:37:28 +0800

    部署首页

---
+++ src/main/java/com/woniuxy/thirdstageproject/ThirdstageprojectApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy.thirdstageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdstageprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdstageprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woniuxy/thirdstageproject/ThirdstageprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woniuxy.thirdstageproject;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdstageprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
